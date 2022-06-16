78046110178a0fb41147f57b4c51bb1c85298dac
boburanvarov <boburanvarov841@gmail.com>
2022-06-16 15:49:40 +0500

    exchenge project17

--- src/app/shared/constants/consts.ts
+++ src/app/shared/constants/consts.ts
@@ -1,3 +1,6 @@
- export  const API = "http://172.20.51.11:2121/api/";
+export const API = 'http://172.20.51.11:2121/api/';
 // export  const API2 = "http://172.20.51.11:9092/api/";
 // export  const API = "http://172.20.51.52:2121/api/";
+
+
+//export const API = 'https://192.168.70.105:2121/api/';
